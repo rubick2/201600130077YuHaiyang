6660b5768b60cfd9d3bcf0f0075b210c380f1434
rubick2 <792251437@qq.com>
2018-09-18 09:23:44 +0800

    haha

--- homework1.py
+++ homework1.py
@@ -2,18 +2,17 @@
 import os
 import re
 
-m = []
+word_list = list()
 root_dir = r"D:\program\python\IRandDM\work1\alt.atheism"
-
 for file in os.listdir(root_dir):
     file_name = root_dir + "\\" + file
     ff = open(file_name, "rb")
 
-    str_a = ff.read()
-    dic_a = str_a.split()
+    letters = re.compile(r'[a-zA-Z]+-?[a-zA-Z]+')
 
-    for word in dic_a:
-        word_a = re.sub(r'\D', "", word.decode())
-        if word_a not in m:
-            m.append(word_a)
-        print(word_a)
+    for line in ff:
+        str_line = str(line).strip().lower()
+        for word in re.findall(letters, str_line):
+            if word not in word_list:
+                word_list.append(word)
+print(word_list)
